490ca66843ce862ceb0c68ca14d9fca9d5f84f5c
2nd-update <ahmedellaban1>
2023-05-03 04:47:50 +0200

    sign-up form & signup.html

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,11 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'password', 'email', 'first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for i in self.fields:
+            self.fields[i].widget.attrs['placeholder']  = self.fields[i].label
+            self.fields[i].label = ''
+            self.fields[i].help_text = ''
+
